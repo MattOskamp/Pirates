ee2d12b5ab9142c7f5d718c18182a4764b6abca2
Matt Oskamp <mattoskamp@gmail.com>
2013-06-25 19:16:50 -0400

    First upload

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+    // Properties
+    public float Name;
+    public float Speed;
+    public float AttackRange;
+    public float CargoSpace;
+    public float Health;
+    public float AttackDamage;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnMouseDown () {
+		// select the unit
+		// display how far it can move/attack
+		
+	}
+	
+}
