401ccd23d3a6c235acd7343b60dbf6c335ede030
Matt Oskamp <mattoskamp@gmail.com>
2013-06-26 10:50:27 -0400

    working on game rules

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -1,18 +1,37 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Ship : MonoBehaviour {
     // Properties
-    public float Name;
-    public float Speed;
-    public float AttackRange;
-    public float CargoSpace;
-    public float Health;
-    public float AttackDamage;
+    public string Name;
+    public int LongMove; // the ratio between long:short is 8.5:5.7cm so approx 3:2
+    public int ShortMove;
+    public int CargoSpace;
+    public int Cost;
+    public int Masts;
+    public List<Cannon> Cannons;
+    public List<Cargo> CargoHold;
+
+    private float moveDistance;
+
+    public struct Cannon
+    {
+        float roll;
+        float longRange;
+        float shortRange;
+    }
+
+    public struct Cargo
+    {
+        float value;
+    }
 
 	// Use this for initialization
 	void Start () {
-	    
+        moveDistance = LongMove * 3 + ShortMove * 2;
+
+
 	}
 	
 	// Update is called once per frame
